355d9fa7edfedbf99e568d7a262020c2a10af282
hhstore <hhglory@outlook.com>
2021-11-01 15:05:20 +0800

    update

--- lib/app/modules/nav/views/nav_view.dart
+++ lib/app/modules/nav/views/nav_view.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 
 import '../controllers/nav_controller.dart';
 
 class NavView extends GetView<NavController> {
+  final _scrollController = ScrollController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,11 +14,28 @@ class NavView extends GetView<NavController> {
         centerTitle: true,
       ),
       body: Center(
-        child: Text(
-          'NavView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
+          child: ListView(
+        controller: _scrollController, // fix
+        shrinkWrap: true,
+        children: [
+          GridView.builder(
+            shrinkWrap: true,
+            //禁用滑动事件
+            scrollDirection: Axis.vertical,
+            physics: NeverScrollableScrollPhysics(),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 6,
+            ),
+            itemBuilder: (context, index) {
+              return Container(
+                height: 5,
+                color: Colors.primaries[index % Colors.primaries.length],
+              );
+            },
+            itemCount: 50,
+          ),
+        ],
+      )),
     );
   }
 }
